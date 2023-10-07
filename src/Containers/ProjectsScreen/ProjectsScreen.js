0202f5a68e1186ed0097f0a22e88ac360b107014
graceelias <91794990+graceelias@users.noreply.github.com>
2023-10-07 17:40:57 -0400

    creating index.html

--- src/Containers/ProjectsScreen/ProjectsScreen.js
+++ src/Containers/ProjectsScreen/ProjectsScreen.js
@@ -1,7 +1,15 @@
 import "./ProjectsScreen.css";
+import React, {useState} from 'react';
 
 function ProjectsScreen()
 {
+    const [projectClicked, setProjectClicked] = useState(String);
+
+    const handlePersonalWebsiteClick = () =>
+    {
+      setProjectClicked("PersonalWebsite");
+    }
+  
     const Button = ({style, text, onClick}) =>
     {
       return(
@@ -13,10 +21,10 @@ function ProjectsScreen()
 
     return(
         <section className="project_container">
-            <Button text="Personal Website" style="personal_website"> </Button>
-            <Button text="FlixFinder" style="flix_finder"> </Button>
-            <Button text="Needle Felting Machine" style="needle_felting_machine"> </Button>
-            <Button text="Infrared Light Head Tracker" style="infrared_light_head_tracker"> </Button>
+            <Button text="Personal Website" style="personal_website"> onClick={handlePersonalWebsiteClick} </Button>
+            <Button text="FlixFinder" style="flix_finder" onClick={handleFlixFinderClick}> </Button>
+            <Button text="Needle Felting Machine" style="needle_felting_machine" onClick={handleNeedleFeltingMachineClick}> </Button>
+            <Button text="Infrared Light Head Tracker" style="infrared_light_head_tracker" onClick={handleInfraredLightHeadTrackerClick}> </Button>
         </section>
     );
 }
